af7a9c695a14ab5edf48bcc9a583e63684fb4788
RubyKobayashi <ruby@rubykobayashi.com>
2019-07-03 13:35:36 +0100

    settling class names dynamically

--- src/App.js
+++ src/App.js
@@ -69,21 +69,29 @@ class App extends Component {
           })}
         </div>
         );
+
         style.backgroundColor = 'red';
+      }
 
-    };
+      let classes = [];
+      if (this.state.persons.length <= 2) {
+        classes.push('red');
+      }
+      if (this.state.persons.length <= 1) {
+        classes.push('bold');
+      }
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        {/*can be inefficient*/}
-        <button
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
-    );
+      return (
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          {/*can be inefficient*/}
+          <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
